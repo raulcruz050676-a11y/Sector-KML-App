3a4239f550602c435bf125a69404516b8091bdf1
raulcruz050676-a11y <raulcruz050676@gmail.com>
2026-03-18 08:10:15 -0300

    Update MainActivity.kt

--- android/app/src/main/kotlin/com/tuapp/kmlcraft/MainActivity.kt
+++ android/app/src/main/kotlin/com/tuapp/kmlcraft/MainActivity.kt
@@ -1,13 +1,12 @@
 package com.tuapp.kmlcraft
 
-// ESTAS SON LAS LÍNEAS QUE EL ROBOT NO ENCUENTRA:
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        
-        // Aquí es donde KML Craft empieza a procesar los datos
+        // ESTA LÍNEA ES LA QUE CONECTA CON EL DISEÑO:
+        setContentView(R.layout.activity_main)
     }
 }
