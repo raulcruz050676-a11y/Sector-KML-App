72f9aa8e245d8dea1a00ce28ba9a7f8137c0ccb4
raulcruz050676-a11y <raulcruz050676@gmail.com>
2026-03-18 10:02:31 -0300

    Update MainActivity.kt

--- android/app/src/main/kotlin/com/tuapp/kmlcraft/MainActivity.kt
+++ android/app/src/main/kotlin/com/tuapp/kmlcraft/MainActivity.kt
@@ -1,11 +1,4 @@
 package com.tuapp.kmlcraft
 
-import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
-
-class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-    }
+class MainActivity {
 }
