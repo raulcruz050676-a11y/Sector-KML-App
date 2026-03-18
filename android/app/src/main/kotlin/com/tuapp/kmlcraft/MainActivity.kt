5adcd34ffc582469edd91592c7493f5e2b193768
raulcruz050676-a11y <raulcruz050676@gmail.com>
2026-03-18 10:47:55 -0300

    Update MainActivity.kt

--- android/app/src/main/kotlin/com/tuapp/kmlcraft/MainActivity.kt
+++ android/app/src/main/kotlin/com/tuapp/kmlcraft/MainActivity.kt
@@ -2,12 +2,13 @@ package com.tuapp.kmlcraft
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-// AGREGÁ ESTA LÍNEA JUSTO ACÁ:
+// ESTA LÍNEA ES EL PUENTE QUE FALTA:
 import com.tuapp.kmlcraft.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        // Usamos la referencia completa para que no haya dudas:
         setContentView(R.layout.activity_main)
     }
 }
