39085b7e175b5d29966e9cd31516c631d25ff8e4
raulcruz050676-a11y <raulcruz050676@gmail.com>
2026-03-18 00:38:58 -0300

    Create MainActivity.kt

---
+++ android/app/src/main/kotlin/com/tuapp/kmlcraft/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.tuapp.kmlcraft
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        // Aquí va el resto de tu lógica de KML
+    }
+}
