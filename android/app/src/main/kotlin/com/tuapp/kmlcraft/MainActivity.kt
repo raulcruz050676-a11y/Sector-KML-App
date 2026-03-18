53e0e275dc9d8a2a5539ecae41328c5ad28671b7
raulcruz050676-a11y <raulcruz050676@gmail.com>
2026-03-18 01:06:58 -0300

    Update MainActivity.kt

--- android/app/src/main/kotlin/com/tuapp/kmlcraft/MainActivity.kt
+++ android/app/src/main/kotlin/com/tuapp/kmlcraft/MainActivity.kt
@@ -1,6 +1,6 @@
 package com.tuapp.kmlcraft
 
-// ESTAS LÍNEAS SON LAS QUE EL ROBOT RECLAMA (LOS IMPORT):
+// ESTAS SON LAS LLAVES QUE EL ROBOT RECLAMA (LOS IMPORT):
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
@@ -8,7 +8,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         
-        // Aquí podés pegar el resto de tu lógica de cálculo de KML
-        // Por ahora lo dejamos simple para que el Build pase de largo
+        // Aquí es donde el motor de KML Craft empieza a trabajar
     }
 }
